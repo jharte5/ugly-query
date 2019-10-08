cb9ed6aa4fd4cebbf6a1a348a704520d9d800549
jharte5 <jonathan.harte@codeimmersives.com>
2019-10-08 18:48:59 -0400

    changed p

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-document.querySelector
+const head = document.querySelector('h1');
+head.style.fontSize  = '80px';
+head.style.fontFamily = 'Comic Sans MS, cursive, sans-serif'
+head.style.color = 'green';
+head.style.opacity = '.2';
+
+const otherHead = document.querySelector('h3');
+otherHead.style.fontSize = '40px';
+otherHead.style.fontFamily = 'Lucida Console,  Monaco, monospace';
+otherHead.style.backgroundColor = 'green';
+
+const paragraph = document.querySelectorAll('p');
+paragraph[0].style.color = 'lightgreen';
+paragraph[1].style.color = 'lightgreen';
+paragraph[2].style.color = 'lightgreen';
+
+
